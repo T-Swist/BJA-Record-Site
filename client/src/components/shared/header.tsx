02649fb7bbf58616dc6560970813da6e14267892
daidaiminah <daidaiminahtswist@gmail.com>
2026-02-19 16:14:00 +0000

    Did some changes on the header

--- client/src/components/shared/header.tsx
+++ client/src/components/shared/header.tsx
@@ -65,7 +65,7 @@ const Header = () => {
         
           <Link 
             to="/" 
-            className=""
+            className="mr-64"
             onClick={() => {
               setIsMobileMenuOpen(false);
               setActiveDropdown(null);
